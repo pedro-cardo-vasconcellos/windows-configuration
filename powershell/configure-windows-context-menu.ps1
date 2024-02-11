68fbe5e1a5465afa20579e282e80bf53714ba008
Pedro Cardo Vasconcellos <pedro.cardo.vasconcellos@gmail.com>
2024-02-11 20:41:07 -0300

    Some fixes

--- powershell/configure-windows-context-menu.ps1
+++ powershell/configure-windows-context-menu.ps1
@@ -1,2 +1,2 @@
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+Invoke-Expression "reg add ""HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32"" /f /ve"
 Stop-Process -ProcessName explorer -Force
